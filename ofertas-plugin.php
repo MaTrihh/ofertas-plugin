4fabcbe6bd6a34d4564d6cd5ff13b08e9b820e98
MaTrihhh <ibai.ocana@gmail.com>
2024-03-18 17:52:06 +0100

    Añadido estilos para la zona de admin

--- ofertas-plugin.php
+++ ofertas-plugin.php
@@ -20,3 +20,11 @@ include_once plugin_dir_path( __FILE__ ) . 'includes/functions/api.php';
 include_once plugin_dir_path( __FILE__ ) . 'includes/Oferta.php';
 
 register_activation_hook( __FILE__, 'ofertas_plugin_crear_tabla' );
+
+function ofertas_plugin_agregar_estilos() {
+    // Enqueue el archivo CSS
+    wp_enqueue_style('op_main', plugins_url( 'includes/styles/main.css', __FILE__ ));
+}
+
+// Para cargar los estilos en el frontend del sitio
+add_action('admin_enqueue_scripts', 'ofertas_plugin_agregar_estilos'); 
